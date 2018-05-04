ff999a27372c43a404e737588c7dde4771d20b24
sachinkreeti <sgupta@kreeti.com>
2018-05-04 18:37:08 +0530

    firebase cloud messaging working code

--- App.js
+++ App.js
@@ -6,32 +6,24 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+import firebase from 'react-native-firebase';
+// import firebase from 'react-native-firebase';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
+    firebase.messaging().getToken()
+      .then((fcmToken) => {
+        console.log('Firebase token ', fcmToken);
+      });
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
         </Text>
       </View>
     );
